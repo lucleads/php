e8c56f8d77862cdb8e5e4f4900202177c1f89024
Lucas D <78828405+lucleads@users.noreply.github.com>
2021-10-04 14:13:46 +0200

    Delete unnecessary methods
    Enum class is just an alias for a possible values container. It should not have a value.

--- Utils/EnumClasses/Enum.php
+++ Utils/EnumClasses/Enum.php
@@ -2,31 +2,11 @@
 
 namespace Utils\EnumClasses;
 
-use Utils\EnumClasses\ValueObjects\StringValue;
-
 /**
  * Class Enum
  * @package Utils\EnumClasses
  */
-abstract class Enum extends StringValue
+abstract class Enum
 {
-    /**
-     * is
-     * @param string|null ...$value
-     * @return bool
-     */
-    public function is(?string ...$value): bool
-    {
-        return in_array($this, $value);
-    }
-
-    /**
-     * isNot
-     * @param string|null ...$value
-     * @return bool
-     */
-    public function isNot(?string ...$value): bool
-    {
-        return !in_array($this, $value);
-    }
+    
 }
